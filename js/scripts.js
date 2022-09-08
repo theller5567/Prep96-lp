f0ec39bead400cdb265058d76583c7d062070bb1
Travis <theller5567@gmail.com>
2022-09-08 15:51:30 -0400

    Completed basic layout

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    // Hamburger menu toggle
+    $('.nav-toggle').on('click', function() {
+        $('.main-nav').toggleClass('is-open');
+        $('.hamburger').toggleClass('is-open');
+    });
+
+
+
+
+});
